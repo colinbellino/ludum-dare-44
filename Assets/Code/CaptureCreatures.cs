1c426cae59ec8f8d371e10bcd7738933b4447d2c
Erehuc <huc.romain@gmail.com>
2019-04-27 21:12:33 +0200

    Kill and Eating 🐓 !

---
+++ Assets/Code/CaptureCreatures.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class CaptureCreatures : MonoBehaviour
+{
+	public Action<CaptureData> CaptureAction = delegate { };
+
+	private void OnGUI()
+	{
+		Event e = Event.current;
+
+		if (e.isKey && e.keyCode == KeyCode.Return)
+		{
+			Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 1f);
+
+			for (int i = 0; i < colliders.Length; i++)
+			{
+				if (colliders[i].transform == transform)
+				{
+					continue;
+				}
+
+				// Destroy captured creature
+				Debug.Log("You successfully capture something, is it a Chicken ?!");
+				GameObject.Destroy(colliders[i].transform.gameObject);
+
+				// Send capture event
+				var captureData = new CaptureData { captured = colliders[i].transform };
+				CaptureAction(captureData);
+			}
+		}
+	}
+}
+
+public class CaptureData
+{
+	public Transform captured;
+}
---
+++ Assets/Code/OnCapture.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCapture : MonoBehaviour
+{
+	private CaptureCreatures captureCreatures;
+	private Health playerHealth;
+
+	private void OnEnable()
+	{
+		captureCreatures = GetComponent<CaptureCreatures>();
+		playerHealth = GetComponent<Health>();
+		captureCreatures.CaptureAction += OnCreatureCaptured;
+	}
+
+	private void OnDisable()
+	{
+		captureCreatures.CaptureAction -= OnCreatureCaptured;
+	}
+
+	private void OnCreatureCaptured(CaptureData data)
+	{
+		switch (data.captured.name)
+		{
+				case "Chicken":
+					playerHealth.AddHealth(10);
+					break;
+				default:
+					Debug.Log("Hey what did you capture ?");
+					break;
+		}
+	}
+}
