808d8d35b960888e6fa0baac899ff596c1d7f0f5
Colin Bellino <bellino.colin@gmail.com>
2019-04-27 19:24:29 +0200

    Fix clamp health

--- Assets/Code/Health.cs
+++ Assets/Code/Health.cs
@@ -1,10 +1,11 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-	[SerializeField] private int _current = 100;
-	[SerializeField] private int _max = 100;
+	[SerializeField] private int _defaultCurrent = 100;
+	[SerializeField] private int _defaultMax = 100;
 
 	public Action HealthChangeAction = delegate { };
 
@@ -13,18 +14,41 @@ public class Health : MonoBehaviour
 
 	private void OnEnable()
 	{
-		max = _max;
-		ClampCurrentHealth(_current);
+		max = _defaultMax;
+		ClampCurrentHealth(_defaultCurrent);
 	}
 
 	private void ClampCurrentHealth(int value)
 	{
-		current = Math.Max(max, Math.Min(0, value));
+		current = Math.Max(0, Math.Min(max, value));
 		HealthChangeAction();
 	}
 
 	public void AddHealth(int value)
 	{
 		ClampCurrentHealth(current + value);
+	}
+}
+
+[CustomEditor(typeof(Health))]
+public class HealthEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		var health = (Health) target;
+
+		if (GUILayout.Button("Add 10 Health"))
+		{
+			Debug.Log("+10");
+			health.AddHealth(10);
+		}
+
+		if (GUILayout.Button("Remove 10 Health"))
+		{
+			Debug.Log("-10");
+			health.AddHealth(-10);
+		}
 	}
 }
