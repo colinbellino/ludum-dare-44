4397cd0b1cfdb5c2da38e70ea35359c96f582f1a
Erehuc <huc.romain@gmail.com>
2019-04-27 16:34:22 +0200

    Level !!

---
+++ Assets/Code/Wall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private void OnEnable()
+    	{
+    		Collisions.TriggerEnterAction += OnTriggerAction;
+    	}
+
+    	private void OnDisable()
+    	{
+    		Collisions.TriggerEnterAction -= OnTriggerAction;
+    	}
+
+    	private void OnTriggerAction(TriggerEnterData data)
+    	{
+    		// if the collision was with me
+    		if (data.other.transform != transform) { return; }
+
+    		Debug.Log("trigger -> " + data.source.name + " / " + data.other.name);
+    	}
+}
