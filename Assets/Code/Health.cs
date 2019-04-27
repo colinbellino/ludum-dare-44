80959ddb2644a81f8615767beabcae95302240d5
Colin Bellino <bellino.colin@gmail.com>
2019-04-27 19:17:01 +0200

    Update Health

--- Assets/Code/Health.cs
+++ Assets/Code/Health.cs
@@ -1,6 +1,30 @@
+using System;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-	public int current = 100;
+	[SerializeField] private int _current = 100;
+	[SerializeField] private int _max = 100;
+
+	public Action HealthChangeAction = delegate { };
+
+	public int current { get; private set; }
+	public int max { get; private set; }
+
+	private void OnEnable()
+	{
+		max = _max;
+		ClampCurrentHealth(_current);
+	}
+
+	private void ClampCurrentHealth(int value)
+	{
+		current = Math.Max(max, Math.Min(0, value));
+		HealthChangeAction();
+	}
+
+	public void AddHealth(int value)
+	{
+		ClampCurrentHealth(current + value);
+	}
 }
